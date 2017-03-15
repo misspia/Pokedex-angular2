c612e2cc4e62e21caea25beba36b18c88e56890a
misspia <pia_leung@hotmail.com>
2017-03-15 00:52:55 -0400

    scraper: master type chart

--- models/scraper.js
+++ models/scraper.js
@@ -10,6 +10,7 @@ function main() {
 
 	getPokemonList(baseUrl);
 	getEvolutionChart(baseUrl + '/evolution');	
+	getMasterTypeChart(baseUrl + '/type/dual');
 }
 
 function requestUrl(url) {
@@ -444,12 +445,81 @@ function burmyEvolutionCase($, tree) {
 			memberInfo['stage1'].push(unGroupedEvolStage($, 1, member, 'burmy'));
 		}		
 	})
-	console.log(JSON.stringify(memberInfo));
+	// console.log(JSON.stringify(memberInfo));
 	return memberInfo;
 }
 
 
+//////////////////// DUAL TYPE CHART ////////////////////////
 
+function getMasterTypeChart(url) {
+	
+	let requestMasterTypeChart = requestUrl(url);
+
+	requestMasterTypeChart.then( body => {
+
+		let $ = cheerio.load(body),
+			masterTable = $('#dualtypechart').find('tbody');
+			
+			scrapeMasterTypeChart($, masterTable);
+
+	}).catch( err => {
+
+		console.log(err);
+	})
+}
+
+function scrapeMasterTypeChart($, master) {
+
+	let typeEffects = [];
+
+	$($(master).children('tr')).map( (index, type) => {
+
+		if($(type).attr('class') == 'hasPkmn' || $(type).attr('class') == 'noPkmn') {
+			typeEffects.push(scrapeTypeRow($, type));
+		}
+	})
+	console.log(typeEffects);
+	return typeEffects;
+}
+
+function scrapeTypeRow($, typeRow) {
+
+	let currentTypes = $(typeRow).find('th');
+	let key = formatTypeKey($, currentTypes);
+	let effectValues = getTypeEffectValues($, typeRow);
+	let type = {};
+
+	type[key] = effectValues;
+
+	return type;
+}
+
+function formatTypeKey($, nameContainer) {
+	
+	let type1 = $(nameContainer).children('a').eq(0).text(),
+		type2 = $(nameContainer).children('a').eq(1).text() == '—' ? '' : '-' + $(nameContainer).children('a').eq(1).text(),
+		name =  type1 + type2;
+	
+	return name.toLowerCase();
+}
+
+function getTypeEffectValues($, typeRow) {
+	
+	let effectCells = $(typeRow).children('.type-fx-cell');
+	let effectsArr = [];
+
+	$(effectCells).map( (index, cell) => {
+		
+		// let effectValue = $(cell).text();
+		let effectValue = $(cell).attr('class').replace('type-fx-cell type-fx-', '');
+
+		effectsArr.push(effectValue);
+	});
+
+	console.log(effectsArr);
+	return effectsArr;
+}
 
 
 
