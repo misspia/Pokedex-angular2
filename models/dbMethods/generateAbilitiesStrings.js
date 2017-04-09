6ebc60753dbc7a3beaa146d97c4f7bf8f3dc2183
Jason Manson-Hing <jason.mansonhing@gmail.com>
2017-04-08 18:54:12 -0700

    Query Generator p2

--- models/dbMethods/generateAbilitiesStrings.js
+++ models/dbMethods/generateAbilitiesStrings.js
@@ -7,7 +7,7 @@ fs.unlink('abilitiesInsertion.txt', cb => {
 	for (let i = 0; i < abilitiesDict.length; i++) {
 
 		// Assuming Name, Pokemon, Description, Generation
-		let formattedString = 'INSERT into abilities VALUES (\'' + abilitiesDict[i].Name + '\', ' + abilitiesDict[i].Pokemon + ', \'' + abilitiesDict[i].Description + '\', ' + abilitiesDict[i].Generation + ');\n';
+		let formattedString = 'INSERT into pokedex.abilities VALUES (\'' + abilitiesDict[i].Name + '\', ' + abilitiesDict[i].Pokemon + ', \'' + abilitiesDict[i].Description + '\', ' + abilitiesDict[i].Generation + ');\n';
 
 		fs.appendFileSync('abilitiesInsertion.txt', formattedString)
 	}
